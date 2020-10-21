cc023660853912461cde13fef2c34c7683fa00e1
Zak <yakdi.zak@gmail.com>
2020-10-21 15:31:13 +0200

    Add JWT authentication

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
+import dotenv from "dotenv";
 import express from "express";
+import jwt from "jsonwebtoken";
 
-const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(3000, () => console.log(`Hello world`));
+app.use(express.json());
+
+app.post("/api/justify", authenticateToken, (req, res) => {
+  res.send(justifyText(req.body.text));
+});
+
+app.post("/api/token", (req, res) => {
+  const { email } = req.body;
+  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET_KEY);
+  res.send({ token });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, email) => {
+    if (err) return res.sendStatus(403);
+    req.email = email;
+    next();
+  });
+}
+
+function justifyText(text) {
+  return text;
+}
